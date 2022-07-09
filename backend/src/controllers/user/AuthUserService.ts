5f4fb5b5bd1c6fca1141a905c6c29e4015a16af2
pedroblome <joaopedroblome78@gmail.com>
2022-07-09 19:49:14 -0300

    jwt token to user login

--- backend/src/controllers/user/AuthUserService.ts
+++ backend/src/controllers/user/AuthUserService.ts
@@ -1,6 +1,7 @@
 import prismaClient from "../../prisma";
-import {compare} from 'bcryptjs'
+import { compare } from "bcryptjs";
 import { userInfo } from "os";
+import { sign } from "jsonwebtoken";
 interface AuthRequest {
   email: string;
   password: string;
@@ -10,20 +11,36 @@ class AuthUserService {
   async execute({ email, password }: AuthRequest) {
     //check if existe the email which is trying to make the login
     const user = await prismaClient.user.findFirst({
-        where :{
-            email: email
-        }
-    })
-    if(!user){
-        throw new Error("user email not found or incorrect!!")
+      where: {
+        email: email,
+      },
+    });
+    if (!user) {
+      throw new Error("user email not found or incorrect!!");
     }
-    const passwordMatch = await compare(password, user.password)
-    if(!passwordMatch){
-      throw new Error("password incorrect!!")
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) {
+      throw new Error("password incorrect!!");
     }
     //genereate a jwt token and show id, name and user
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "30d",
+      }
+    );
 
-    return { ok: true };
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.name,
+      token: token,
+    };
   }
 }
 export { AuthUserService };
