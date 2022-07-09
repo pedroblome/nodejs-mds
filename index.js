7b3edcc0cfb37db12e79df19f6600098cb916321
pedroblome <joaopedroblome78@gmail.com>
2022-07-09 00:19:09 -0300

    creating function middlewares, betwen requisition and final response in frontend

--- index.js
+++ index.js
@@ -9,17 +9,41 @@ server.use(express.json());
 
 const cursos = ["NOde js", "javascript", "reactnative"];
 
-server.get("/cursos", (req, res) => {
+//middleware global
+
+server.use((req,res, next ) =>{
+  console.log(`url chamada ${req.url}`)
+  return next();
+
+})  
+
+function checkCurso(req,res,next){
+  if(!req.body.name){
+    return res.status(400).json({ error: "Nome do curso e obrigatorio!!"})
+  }
+  return next();
+}
+
+function checkIndexCurso(req,res,next){
+  const curso = req.params.index
+  const atualLengt = cursos.length
+  if(curso<0 || curso>(atualLengt-1) ){
+    return res.status(400).json({error: "index do curso nao existe, revise por favor!!"})
+  }
+  return next();
+}
+
+server.get("/cursos", checkIndexCurso, (req, res) => {
   return res.json(cursos);
 });
 
-server.get("/cursos/:index", (req, res) => {
+server.get("/cursos/:index",checkIndexCurso, (req, res) => {
   const { index } = req.params;
 
   return res.json(cursos[index]);
 });
 
-server.post("/cursos", (req, res) => {
+server.post("/cursos", checkCurso, (req, res) => {
   // the line above say that the property that will be sended is a body json
   const { name } = req.body;
   cursos.push(name);
@@ -27,7 +51,7 @@ server.post("/cursos", (req, res) => {
   return res.json(cursos);
 });
 
-server.put("/cursos/:index", (req,res) =>{
+server.put("/cursos/:index", checkCurso,checkIndexCurso, (req,res) =>{
   const {index} =  req.params;
   const {name} = req.body;
 
@@ -38,7 +62,7 @@ server.put("/cursos/:index", (req,res) =>{
    
 })
 
-server.delete("/cursos/:index", (req,res) =>{
+server.delete("/cursos/:index",checkIndexCurso, (req,res) =>{
   const {index} =  req.params;
 
   cursos.splice(index, 1)
