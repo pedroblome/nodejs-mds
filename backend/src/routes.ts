9dbecf4d4e57839bee7d44a16185c979aef1c898
pedroblome <joaopedroblome78@gmail.com>
2022-07-09 10:53:22 -0300

    Errors treatment

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,9 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/teste", (req: Request, res:Response) => {
-  return res.json({ ok: "sujeito pizza" });
+  throw new Error(" nome: pizza de calabresa com frango")
 });
 
-export {router}
+export {router} 
+
+
