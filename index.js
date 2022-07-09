cf9c3ded29956c4d04bd177f6087eb2bb2f10390
pedroblome <joaopedroblome78@gmail.com>
2022-07-08 23:41:22 -0300

    crud requisitions

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 const express = require("express");
 
+//used to
+// server.use(express.json());
+
 const server = express();
 
-// Query params =?nome=NodeJS
-// Route Params = /curso/2
-// Request Body = {nome: 'NodeJS', tipo:'BackEnd'}
+server.use(express.json());
 
-const curso = ['NOde js', 'javascript', 'reactnative', 'reactnative', 'react  native', 'react  native', 'react  native', 'react  native']
+const cursos = ["NOde js", "javascript", "reactnative"];
 
-server.get('/curso/:index', (req, res) =>{
-    const {index} = req.params;
-    
-    return res.json(curso[index]);
+server.get("/cursos", (req, res) => {
+  return res.json(cursos);
+});
+
+server.get("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+
+  return res.json(cursos[index]);
+});
+
+server.post("/cursos", (req, res) => {
+  // the line above say that the property that will be sended is a body json
+  const { name } = req.body;
+  cursos.push(name);
+
+  return res.json(cursos);
+});
+
+server.put("/cursos/:index", (req,res) =>{
+  const {index} =  req.params;
+  const {name} = req.body;
+
+  cursos[index] = name;
+
+  return res.json(cursos);
+
+   
+})
+
+server.delete("/cursos/:index", (req,res) =>{
+  const {index} =  req.params;
+
+  cursos.splice(index, 1)
+
+  return res.json(cursos)
+
 })
 
 server.listen(3000);
