fca756a3c2460d99fc496fb5c19b77c7a12745e3
pedroblome <joaopedroblome78@gmail.com>
2022-07-17 22:21:38 -0300

    route of product created

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
-import  {isAuthenticated} from "./middlewares/isAuthenticated"
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 const router = Router();
 
 // -- user routes --
@@ -12,12 +13,17 @@ router.post("/users", new CreateUserController().handle);
 
 router.post("/session", new AuthUserController().handle);
 
-router.get("/me", isAuthenticated, new DetailUserController().handle)
-
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 // -- category routes --
-router.post("/category", isAuthenticated, new CreateCategoryController().handle)
-router.get("/category", isAuthenticated, new ListCategoryController().handle)
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
+router.get("/category", isAuthenticated, new ListCategoryController().handle);
 
+// -- product routes --
+router.post("/product", isAuthenticated, new CreateProductController().handle);
 
 export { router };
